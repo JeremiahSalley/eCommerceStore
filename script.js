bd472a9d6f999af8e015b3127d09e44b612dbde9
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2022-03-26 16:32:58 -0600

    added icons, newsletter and footer

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const url = 'https://fakestoreapi.com/products'
+const url = 'https://fakestoreapi.com/products/categories'
 
-fetch(url)
-    .then(res => {
-        console.log('succes', res)
-    })
+function displayProduct(){
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
     .catch(err => {
         console.log('something is wrong', err)
     })
+}
 
-          
+displayProduct()
+
+
+let icon = document.getElementsByClassName('icon')[0]
+
+console.log(icon)
+
+icon.addEventListener('click', iconClicked)
+
+function iconClicked(){
+    console.log('here')
+}
+
+
+
+
