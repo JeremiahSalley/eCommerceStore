5dda85c3a8a8cec4f99746b80e4a4b634cefaac1
Jeremiah Salley <Jeremiah.salley@yahoo.com>
2022-04-10 10:37:05 -0600

    added product from api and styled the product

--- script.js
+++ script.js
@@ -1,27 +1,118 @@
-const url = 'https://fakestoreapi.com/products/categories'
+const url = "https://fakestoreapi.com/products";
+const mensUrl = "https://fakestoreapi.com/products/category/men's clothing";
+const womenUrl = "https://fakestoreapi.com/products/category/women's clothing";
+const electronicsUrl = "https://fakestoreapi.com/products/category/electronics";
+const jeweleryUrl = "https://fakestoreapi.com/products/category/jewelery";
 
-function displayProduct(){
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => {
-        console.log('something is wrong', err)
+function womenData(){
+  fetch(womenUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let femaleData = "";
+      data.map((values) => {
+        femaleData += `<div class="card womenCards">
+        <div class="imageContainer">
+          <img class="shop-item-image" src="${values.image}">
+        </div>
+        <div class="shopDetails">
+          <h5 class="name">${values.title}</h5>
+          <p class="price">${values.price}</p>
+          <button class="add-cart-btn">Add To Bag</button>
+        </div>
+      </div>`
+      });
+      console.log(femaleData)
+      document.getElementById("womenProducts").innerHTML = femaleData;
     })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-displayProduct()
+function menData(){
+  fetch(mensUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    let maleData = "";
+    data.map((values) => {
+      maleData += `<div class="card">
+      <div class="imageContainer">
+        <img class="shop-item-image" src="${values.image}">
+      </div>
+      <div class="shopDetails">
+        <h5 class="name">${values.title}</h5>
+        <p class="price">${values.price}</p>
+        <button class="add-cart-btn">Add To Bag</button>
+      </div>
+    </div>`
+    });
+    console.log(maleData)
+    document.getElementById("menProducts").innerHTML = maleData;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
 
 
-let icon = document.getElementsByClassName('icon')[0]
+function jeweleryData(){
+  fetch(jeweleryUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    let jewlData = "";
+    data.map((values) => {
+      jewlData += `<div class="card">
+      <div class="imageContainer">
+        <img class="shop-item-image" src="${values.image}">
+      </div>
+      <div class="shopDetails">
+        <h5 class="name">${values.title}</h5>
+        <p class="price">${values.price}</p>
+        <button class="add-cart-btn">Add To Bag</button>
+      </div>
+    </div>`
+    });
+    console.log(jewlData)
+    document.getElementById("jewerelyProducts").innerHTML = jewlData;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
 
-console.log(icon)
 
-icon.addEventListener('click', iconClicked)
-
-function iconClicked(){
-    console.log('here')
+function electronicsData(){
+  fetch(electronicsUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    let tronicsData = "";
+    data.map((values) => {
+      tronicsData += `<div class="card">
+      <div class="imageContainer">
+        <img class="shop-item-image" src="${values.image}">
+      </div>
+      <div class="shopDetails">
+        <h5 class="name">${values.title}</h5>
+        <p class="price">${values.price}</p>
+        <button class="add-cart-btn">Add To Bag</button>
+      </div>
+    </div>`
+    });
+    console.log(tronicsData)
+    document.getElementById("electronicsProducts").innerHTML = tronicsData;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 }
 
 
 
 
+menData()
+
+womenData()
+
+jeweleryData()
+
+electronicsData()
